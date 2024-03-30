6f4a972711b5a13ecf710eb8876cbe09ef29d2e1
Dana Hrivnáková <dada.hrivnakova14@gmail.com>
2024-03-30 09:52:06 +0100

    feedback, onboarding

--- scripts/inboarding.js
+++ scripts/inboarding.js
@@ -1,19 +1,46 @@
 var explainedAI = false;	
 
-function inboarding(element) {
-    if(explainedAI) {
-        seenImage(element);
+function startInboarding() {
+    if(explainedAI == false) {
+        let welcome = document.querySelector('#welcome-to-run-annotation');
+        welcome.style.display = "flex";
     }
-    else {
-        explainedAI = true;
-        openModal();
-        seenImage(element);
+    explainedAI = true;
+}
 
-        var dropdown = document.getElementById('run-annotation');
-        var dropdownMenu = dropdown.querySelector('.dropdown-menu');
+function stopInboarding() {
+    let welcome = document.querySelector('#welcome-to-run-annotation');
+    welcome.style.display = "none";
 
-        dropdownMenu.style.display = 'block';
-    }
+    let dropdown = document.querySelector('#dropdownMenuButton');
+    dropdown.classList.remove('pulse');
+    let dot = document.querySelector('.notification-dot');
+    dot.style.display = "none";
+}
+
+function inboarding() {
+    let welcome = document.querySelector('#welcome-to-run-annotation');
+    welcome.style.display = "none";
+
+    let pulsing_btn = document.querySelector('#dropdownMenuButton');
+    pulsing_btn.classList.remove('pulse');
+    let dot = document.querySelector('.notification-dot');
+    dot.style.display = "none";
+    
+    openModal();
+    
+    let dropdown = document.querySelector('#run-annotation');
+    dropdown.style.display = "block";
+    
+    let btn_run_annotation = document.querySelector('#btn-run-annotation');
+    btn_run_annotation.style.backgroundColor = "#006CEB";
+    
+    let statisitcs = document.querySelector('#statistics');
+    statisitcs.style.display = "none";
+    
+    let image = document.querySelector('#image-annotations');
+    image.style.display = "block";
+    document.getElementById('mainImage').src = "../images/Tv17.png";
 }
 
 var modal = document.getElementById("modal");
@@ -25,13 +52,28 @@ function openModal() {
 }
 
 function closeModal() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
     modal.style.display = "none";
-  }
+  
+    var mainImage = document.getElementById('mainImage');
+    mainImage.style.border = "none";
+  
+    let dropdown = document.querySelector('#run-annotation');
+    dropdown.style.display = "none";
+    
+    let btn_run_annotation = document.querySelector('#btn-run-annotation');
+    btn_run_annotation.style.backgroundColor = "transparent";
+
+    var listAnnotations = document.querySelector('.uploaded-image-annotations.active');
+    listAnnotations.style.border = "none";
+
+    let chips = document.querySelectorAll('.chip');
+    for (var i = 0; i < chips.length; i++) { 
+        chips[i].style.border = "none";
+    }
+
+    var annotationInfo = document.getElementById('annotation-info');
+    annotationInfo.style.borderTop= "none";
+    annotationInfo.style.borderBottom = "0.8px solid rgb(45, 45, 66)";
 }
 
 function seenImage(element) {
@@ -39,7 +81,6 @@ function seenImage(element) {
     document.getElementById('mainImage').src = imgSrc
 
     var parentElement = element.parentNode;
-    console.log(parentElement)
     parentElement.classList.add('seen');    
     
     var annotationsDiv = element.parentElement.nextElementSibling;
@@ -62,8 +103,8 @@ function seenImage(element) {
     }
 }
 
-var currentStep = 1;
 
+var currentStep = 1;
 function nextStep() {
     
     var modalBody = document.querySelector('.modal-body p');
@@ -72,15 +113,27 @@ function nextStep() {
     if(currentStep == 0) {
         modalBody.innerHTML = 'To activate automatic annotation, simply select "<b>Run Annotation</b>" in dropdown menu. <br><br>Our AI technology will then swiftly analyze the current image, highlighting celia for further review.';
         modalFooter.textContent = "1 of 8";
-        
+
         currentStep = 1;
     }
     else if(currentStep == 1) {
-        console.log(currentStep, "aaa");
-        modalBody.innerHTML = "This is the <b>image with the annotated cilia.";
+        modalBody.innerHTML = "This is the <b>image</b> with the annotated cilia.";
+        
+        let back = document.getElementById('backStep');
+        back.style.display = "block";
 
         var mainImage = document.getElementById('mainImage');
         mainImage.style.border = "5px solid #006CEB";
+        
+        let dropdown = document.querySelector('#run-annotation');
+        dropdown.style.display = "none";
+    
+        let btn_run_annotation = document.querySelector('#btn-run-annotation');
+        btn_run_annotation.style.backgroundColor = "transparent";
+
+        let modal = document.getElementById('modal');
+        modal.style.top = "150px"
+        modal.style.left = "850px"
 
         modalFooter.textContent = "2 of 8";
         
@@ -92,6 +145,10 @@ function nextStep() {
         var mainImage = document.getElementById('mainImage');
         mainImage.style.border = "none";
 
+        let modal = document.getElementById('modal');
+        modal.style.top = "250px"
+        modal.style.left = "300px"
+
         var listAnnotations = document.querySelector('.uploaded-image-annotations.active');
         listAnnotations.style.border = "2px solid #006CEB";
 
@@ -99,6 +156,11 @@ function nextStep() {
     } else if(currentStep == 3) {
         modalBody.innerHTML = "This is the <b>confidence level</b> of the annotated cilia. <br><br> If the confidence level is <b>high</b>, the annotation is likely accurate. <br><br> If the confidence level is <b>low</b>, the annotation may not be accurate and needs to be revalidated.";
         modalFooter.textContent = "4 of 8";
+
+        let chips = document.querySelectorAll('.chip');
+        for (var i = 0; i < chips.length; i++) { 
+            chips[i].style.border = "2px solid #006CEB";
+        }
 
         var listAnnotations = document.querySelector('.uploaded-image-annotations.active');
         listAnnotations.style.border = "none";
@@ -108,35 +170,95 @@ function nextStep() {
         modalBody.innerHTML = "Select an annotation to <b>view the details</b> and <b>edit the annotation</b> by clicking on the row.";
         modalFooter.textContent = "5 of 8";
 
+        let chips = document.querySelectorAll('.chip');
+        for (var i = 0; i < chips.length; i++) { 
+            chips[i].style.border = "none";
+        }
+
         currentStep = 5;
     } else if(currentStep == 5) {
         modalBody.innerHTML = "Here are details of the selected annotation. <br><br> You can <b>edit the annotation</b> by selecting the correct microtubular defect or dynein arms from the dropdown menu.";
         modalFooter.textContent = "6 of 8";
 
+        let modal = document.getElementById('modal');
+        modal.style.top = "335px"
+        modal.style.left = "850px"
+
         var annotationInfo = document.getElementById('annotation-info');
-        annotationInfo.style.border = "2px solid #006CEB";
+        annotationInfo.style.borderTop = "0.8px solid #006CEB";
+        annotationInfo.style.borderBottom = "0.8px solid #006CEB";
 
         currentStep = 6;
     } else if(currentStep == 6) {
         modalBody.innerHTML = "You can select annotation by clicking on the image as well.";
         modalFooter.textContent = "7 of 8";
 
+        let modal = document.getElementById('modal');
+        modal.style.top = "150px"
+        modal.style.left = "850px"
+
         var annotationInfo = document.getElementById('annotation-info');
-        annotationInfo.style.border = "none";
+        annotationInfo.style.borderTop= "none";
+        annotationInfo.style.borderBottom = "0.8px solid rgb(45, 45, 66)";
 
         currentStep = 7;
     } else if (currentStep == 7) {
-        modalBody.innerHTML = "You have successfully completed the tutorial. <br><br> Click on the <b>Next</b> button to continue.";
+        modalBody.innerHTML = "You have successfully completed the tutorial. <br><br> Click on the <b>Done</b> button to continue.";
         modalFooter.textContent = "8 of 8";
 
+        let modal = document.getElementById('modal');
+        modal.style.top = "40%"
+        modal.style.left = "40%"
+
         currentStep = 8;
-        var nextButton = document.getElementById('nextButton');
+        var nextButton = document.getElementById('nextStep');
         nextButton.textContent = "Done";
+    } else if(currentStep == 8) {
+        nextButton.onclick = closeModal();
     }
 }
 
 function backStep() {
     currentStep -= 2;
-    console.log(currentStep)
     nextStep();
+
+    if(currentStep < 8) {
+        var nextButton = document.getElementById('nextStep');
+        nextButton.textContent = "Next";
+    }
+
+    if(currentStep == 5) {
+        var annotationInfo = document.getElementById('annotation-info');
+        annotationInfo.style.borderTop = "none";
+        annotationInfo.style.borderBottom = "none";
+
+        let modal = document.getElementById('modal');
+        modal.style.top = "250px"
+        modal.style.left = "300px"
+    }
+    else if(currentStep == 3) {
+        let chips = document.querySelectorAll('.chip');
+        for (var i = 0; i < chips.length; i++) { 
+            chips[i].style.border = "none";
+        }
+    } else if(currentStep == 1) {
+        let back = document.getElementById('backStep');
+        back.style.display = "none";
+
+        let dropdown = document.querySelector('#run-annotation');
+        dropdown.style.display = "block";
+
+        let btn_run_annotation = document.querySelector('#btn-run-annotation');
+        btn_run_annotation.style.backgroundColor = "#006CEB";
+
+        let modal = document.getElementById('modal');
+        modal.style.top = "250px";
+        modal.style.left = "550px";
+
+        let image = document.getElementById('mainImage');
+        image.style.border = "none";
+    } else if(currentStep == 2) {
+        var listAnnotations = document.querySelector('.uploaded-image-annotations.active');
+        listAnnotations.style.border = "none";
+    }
 }
--- scripts/select.js
+++ scripts/select.js
@@ -3,17 +3,3 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("mtds-tool-dropdown-content").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn') && !event.target.matches('.dropbtn *')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
