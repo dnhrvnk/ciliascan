92fca28e862eff0e0e7aed53f447d7c4180c9abb
Dana Hrivnáková <dada.hrivnakova14@gmail.com>
2024-04-07 16:31:50 +0200

    min fix

--- scripts/annotate.js
+++ scripts/annotate.js
@@ -156,6 +156,7 @@ const select_annotation = (annot) => {
   annot_row.parentElement.parentElement.classList.add('selected-row');
 
   update_rightside(annot_row.parentElement.parentElement)
+  document.getElementById("decision-tree-collapsible").style.display = "block";
 }
 
 
